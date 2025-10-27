be079cca5407175d64912f0eeb0ccf073d97dfdf
Anna Keeler <a-keeler@onu.edu>
2025-10-27 18:43:30 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+/*
+* File: P8-2
+* Author: Anna Keeler
+* Date: 10/27/2025
+*
+* Description: Lab Problem 8 part two: commiting.
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main(void) {
+
+	return 0;
+}
+
