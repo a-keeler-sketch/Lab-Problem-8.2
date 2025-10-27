4dfbaa3f0224d47fc9d1e662ad8962e1e5c8046e
Anna Keeler <a-keeler@onu.edu>
2025-10-27 19:08:23 -0400

    Completed P8-2.

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 using namespace std;
 //prototype
 
@@ -15,14 +16,20 @@ int countWords(string str);
 
 //global variable
 
-string input = "string string string"; 
+string input; // get correct word count for however many spaces
+
 int main(void) {
 
-    
-   
+    //DELETE EVERYTHING BEFORE FIRST COMMIT
+    //DELETE THIS FOR THE GITHUB COMMIT, YOU WILL NEED IT LATER THO TO GET USER DATA
+    while (true) {
+        cout << "Enter a string or Q to quit: ";
+        getline(cin, input);
+        if (input == "Q") break;
+
 
         cout << "Word Count: " << countWords(input) << endl;
-    
+    }
        
     return 0;
 
